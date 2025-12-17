7c119ec8a71be6acb8ad7fe96950e1fe37100a37
miguelsoldado12 <miguelsoldado12@gmail.com>
2025-12-17 12:38:09 +0000

    day 2 part 2 + clean up typo in part 1

--- day_4/b.ts
+++ day_4/b.ts
@@ -9,25 +9,31 @@ function canAccess(x: number, y: number) {
 
   for (let dx = Math.max(x - 1, 0); dx <= Math.min(x + 1, width - 1); dx++) {
     for (let dy = Math.max(y - 1, 0); dy <= Math.min(y + 1, height - 1); dy++) {
-      if (lines[dy][dx] === "@") {
-        count++;
-      }
-
+      if (lines[dy][dx] === "@") count++;
       if (count > 4) return false;
     }
   }
 
-  return count <= 4;
+  if (count <= 4) {
+    lines[y] = lines[y].substring(0, x) + "." + lines[y].substring(x + 1);
+    return true;
+  }
 }
 
 let total = 0;
+let accumulator = total;
 
-for (let i = 0; i < width; i++) {
-  for (let j = 0; j < height; j++) {
-    if (lines[j][i] === "@" && canAccess(i, j)) {
-      total++;
+while (true) {
+  for (let i = 0; i < width; i++) {
+    for (let j = 0; j < height; j++) {
+      if (lines[j][i] === "@" && canAccess(i, j)) {
+        total++;
+      }
     }
   }
+
+  if (accumulator === total) break;
+  accumulator = total;
 }
 
 console.log(total);
